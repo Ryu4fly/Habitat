62e6d2d86b5637de8920b1288d082e556223a303
Ryugi Suguro <rsuguro@gmail.com>
2021-02-13 13:22:22 +0900

    user seed and validations created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying all data 📀 ..."
+
+User.destroy_all
+
+puts "Generating new users..."
+10.times do
+  user = User.new
+  user.email = Faker::Internet.email #=> "kirsten.greenholt@corkeryfisher.info"
+  user.password = '123456'
+  user.username = Faker::Name.unique.name # This will return a unique name every time it is called
+  user.avg_cig = 8
+  user.days_smoke_free = rand 30
+  user.cost_a_pack = rand 450..510
+  user.save
+  puts "#{user.id} - #{user.email} created.."
+end
+
+
+puts "All users saved"
