d7b48fc1d3d39c19e4bba09aebdcec102037ba1c
Chiara Campalani <campalanichiara@gmail.com>
2021-02-13 12:58:53 +0900

    added validations to Entry model

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,16 @@
 class EntriesController < ApplicationController
+
+  def index
+    @entries = Entry.all
+  end
+
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
+  private
+
+  def entries_params
+    params.require(:review).permit(:date, :craving, :feeling, :user_id)
+  end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,9 @@
 class Entry < ApplicationRecord
   belongs_to :user
+
+  FEELINGS = ['stressed', 'lonely', 'happy', 'bored', 'excited', 'down', 'angry', 'anxious']
+
+  validates :craving, numericality: { only_integer: true }, presence: true, inclusion: (1..10)
+  validates :date, presence: true
+  validates :feeling, presence: true, inclusion: FEELINGS
 end
