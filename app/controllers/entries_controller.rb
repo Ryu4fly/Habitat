b70ef406c87c97126b6294b042dac2d63b713275
Chiara Campalani <campalanichiara@gmail.com>
2021-02-13 13:22:40 +0900

    added validations to Entry model & created entry index and show view files

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,15 @@
 class EntriesController < ApplicationController
+  def index
+    @entries = Entry.available
+  end
+
+  def show
+    @entry = Entry.find(entry_param)
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:date, :feeling, :craving, :user_id)
+  end
 end
