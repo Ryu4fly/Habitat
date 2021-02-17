5866fdcf520e306db4ed7c271b806e5fbfe0c704
Chiara Campalani <campalanichiara@gmail.com>
2021-02-17 18:41:04 +0900

    added form to create new entry & migrations to add missing data to Entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,6 +2,10 @@ class Entry < ApplicationRecord
   belongs_to :user
 
   FEELINGS = ['stressed', 'lonely', 'happy', 'bored', 'excited', 'down', 'angry', 'anxious']
+  CONTEXT = ["I've just had an argument", "I've just finished eating", "I'm drinking a glass of alcohol", "I want to relax",
+             "I'm at a concert", "i'm going to go to bed", "I'm taking a break", "I'm in a car", "I'm drinking a cup of coffee",
+             "I'm at the phone", "I've made love", "I'm drinking a cup of tea", "I'm watching television", "I've just woken up", 
+             "I'm working", "i'm in a bar", "I'm celebrating something", "I'm partying", "I'm with smokers", "I want to keep my hands busy", "I'm restless", "i'm missing the smell of cigarettes", "I'mmissing the taste of cigarettes", "I'm touching an object related to tobacco", "I'm taking a walk", "I'm hungry", "Nothing special", "Other"]
 
   validates :craving, numericality: { only_integer: true }, presence: true, inclusion: (1..10)
   validates :date, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_031437) do
+ActiveRecord::Schema.define(version: 2021_02_17_093831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2021_02_13_031437) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "cig_smoked"
+    t.string "context"
     t.index ["user_id"], name: "index_entries_on_user_id"
   end
 
