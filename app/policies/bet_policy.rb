1fd9a2fcecea54f940f1b0a4d89d70524ea9db9f
Joshua Hume <valencelectron@hotmail.com>
2021-02-25 15:04:42 +0900

    added bets controller

---
+++ app/policies/bet_policy.rb
@@ -0,0 +1,31 @@
+class BetPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    return true if user
+  end
+
+  def show?
+    return true if user
+  end
+
+  def create?
+    return true if user
+  end
+
+  def show?
+    return true if user
+  end
+
+  def update?
+    return true if user
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
