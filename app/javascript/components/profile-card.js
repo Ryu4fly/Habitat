a8565c210880dc7367ce0671abd77274c92f8db7
Ryugi Suguro <rsuguro@gmail.com>
2021-02-27 14:19:41 +0900

    got the elements overlapping them

--- app/javascript/components/profile-card.js
+++ app/javascript/components/profile-card.js
@@ -5,9 +5,13 @@ const cardsNavigation = () => {
   tabs.forEach(tab => {
     tab.addEventListener('click', () => {
       const target = document.querySelector(tab.dataset.tabTarget);
+      tabs.forEach(tab => {
+        tab.style.zIndex = "0";
+      })
       tabContents.forEach(tabContent => {
         tabContent.classList.remove('active');
       })
+      tab.style.zIndex = "1";
       target.classList.add("active");
     });
   });
